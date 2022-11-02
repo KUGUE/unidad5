394ec118c6a16d57b400afb4654adb876a700e22
Emmanuel Kugue Tapiz <emmanuelk_@alu.uabcs.mx>
2022-11-02 14:38:10 -0700

    14.- Tarea: para el módulo CLIENTE, diseña las funciones índex, show, créate, store

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -14,8 +14,7 @@ class ClientController extends Controller
      */
     public function index()
     {
-        //return Client::all();
-        return Client::with('reservations')->get();
+        echo "Prueba Client Controller ";
     }
 
     /**
@@ -25,7 +24,7 @@ class ClientController extends Controller
      */
     public function create()
     {
-        //
+        return view("create_client");
     }
 
     /**
@@ -36,7 +35,7 @@ class ClientController extends Controller
      */
     public function store(Request $request)
     {
-        //
+     
     }
 
     /**
@@ -47,40 +46,7 @@ class ClientController extends Controller
      */
     public function show($id)
     {
+          echo $id;
        
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Client  $client
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Client $client)
-    {
-       
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Client  $client
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Client $client)
-    {
-       
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Client  $client
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Client $client)
-    {
-        
     }
 }
